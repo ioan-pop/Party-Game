547d70aed9e8f2cab8d5ede3639aac56b99f7172
Ioan Pop <ioan@verified.best>
2020-09-21 22:17:49 -0400

    Updates

---
+++ precache-manifest.47c076e4df8e93e00029fe4cf45157c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de76b6f777ad1cf8e0eeea350e133e97",
+    "url": "/Party-Game/index.html"
+  },
+  {
+    "revision": "99d1feb23a142ba85e1c",
+    "url": "/Party-Game/static/css/main.b4fd9e2b.chunk.css"
+  },
+  {
+    "revision": "ceba80a90a4da4982cd0",
+    "url": "/Party-Game/static/js/2.20873abd.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/Party-Game/static/js/2.20873abd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99d1feb23a142ba85e1c",
+    "url": "/Party-Game/static/js/main.64978f2d.chunk.js"
+  },
+  {
+    "revision": "23343d66162c83cc753a",
+    "url": "/Party-Game/static/js/runtime-main.2ff83781.js"
+  }
+]);
